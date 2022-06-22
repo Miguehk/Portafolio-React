aa8348b122d445ab29fb24d7bd7db51930a89211
Miguel Paniagua <mistergamer180@gmail.com>
2022-06-22 14:11:23 -0600

    Agregados

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,65 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md' 
+import {RiMessengerLine} from 'react-icons/ri'
+import {BsWhatsapp} from 'react-icons/bs'
+import { useRef } from 'react'
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_owbqung', 'template_xv11dsy', form.current, 'VsYz3sQuC8kuAmDPx')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+
+      e.target.reset();
+
+      alert("Submitted successfully");
+  };
   return (
-    <div>Contact</div>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+              <article className="contact__option">
+                  <MdOutlineEmail className='contact__option-icon'/>
+                  <h4>Email</h4>
+                  <h5>mpaniagua@comexstudio.com</h5>
+                  <a target="_blanck" href="mailto:mpaniagua@comexstudio.com">Send a message</a>
+              </article>
+
+              <article className="contact__option">
+                  <RiMessengerLine className='contact__option-icon'/>
+                  <h4>Messager</h4>
+                  <h5>Miguel Paniagua</h5>
+                  <a target="_blanck" href="https://m.me/migueleduado.paniaguasuarez.9">Send a message</a>
+              </article>
+
+              <article className="contact__option">
+                  <BsWhatsapp className='contact__option-icon'/>
+                  <h4>WhatsApp</h4>
+                  <h5>+71263513</h5>
+                  <a target="_blanck" href="https://api.whatsapp.com/send?phone=50671263513&amp;text=Hola!&nbsp;me&nbsp;podes&nbsp;ayudar?">Send a message</a>
+              </article>
+        </div>
+        {/* END OF CONTACT OPTIONS */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name'  placeholder='Your Full Name ' required/>
+          <input type="email"  name='email' placeholder='Your Email' required/>
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/components/testimonials/Testimonilas.jsx
+++ src/components/testimonials/Testimonilas.jsx
@@ -1,9 +1,96 @@
 import React from 'react'
 import './testimonials.css'
+import AVTR1 from '../../assets/avatar1.jpg'
+import AVTR2 from '../../assets/avatar2.jpg'
+import AVTR3 from '../../assets/avatar3.jpg'
+import AVTR4 from '../../assets/avatar4.jpg'
+
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+
 
 const Testimonilas = () => {
   return (
-    <div>Testimonilas</div>
+    <section id='testimonials'>
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper className="container testimonials__container"
+      // install Swiper modules
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={30}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+      >
+          <SwiperSlide className="testimonial">
+              <div className="client__avatar">
+                <img src={AVTR1} alt="Avatar One" />
+              </div>
+              <h5 className='client__name'>Ernest Achiever</h5>
+                <small className='client__review'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                  Similique, quidem iste maxime optio corporis exercitationem 
+                  deserunt obcaecati autem eum illo, dolor ipsum voluptatum unde 
+                  minus molestiae earum cumque provident doloribus!
+                </small>
+            </SwiperSlide>
+
+            <SwiperSlide className="testimonial">
+              <div className="client__avatar">
+                <img src={AVTR2} alt="Avatar One" />
+              </div>
+              <h5 className='client__name'>Ernest Achiever</h5>
+                <small className='client__review'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                  Similique, quidem iste maxime optio corporis exercitationem 
+                  deserunt obcaecati autem eum illo, dolor ipsum voluptatum unde 
+                  minus molestiae earum cumque provident doloribus!
+                </small>
+            </SwiperSlide>
+
+
+            <SwiperSlide className="testimonial">
+              <div className="client__avatar">
+                <img src={AVTR3} alt="Avatar One" />
+              </div>
+              <h5 className='client__name'>Ernest Achiever</h5>
+                <small className='client__review'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                  Similique, quidem iste maxime optio corporis exercitationem 
+                  deserunt obcaecati autem eum illo, dolor ipsum voluptatum unde 
+                  minus molestiae earum cumque provident doloribus!
+                </small>
+            </SwiperSlide>
+
+            <SwiperSlide className="testimonial">
+              <div className="client__avatar">
+                <img src={AVTR4} alt="Avatar One" />
+              </div>
+              <h5 className='client__name'>Ernest Achiever</h5>
+                <small className='client__review'>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                  Similique, quidem iste maxime optio corporis exercitationem 
+                  deserunt obcaecati autem eum illo, dolor ipsum voluptatum unde 
+                  minus molestiae earum cumque provident doloribus!
+                </small>
+            </SwiperSlide>
+
+
+      </Swiper>
+    </section> 
   )
 }
 
